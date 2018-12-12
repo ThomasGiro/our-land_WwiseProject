4b67442b02001ace32350b4a0100102140632911
Raphaël M <raphael.marchetti.pro@gmail.com>
2018-12-12 16:09:02 +0100

    Add wwise ID

---
+++ Wwise_IDs.h
@@ -0,0 +1,63 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_DESERT_PL = 4137235956U;
+        static const AkUniqueID PLAY_END_TURN_PL = 1313846872U;
+        static const AkUniqueID PLAY_MUSIC = 2932040671U;
+        static const AkUniqueID PLAY_PLACEHOLDER_INTEGRATION = 3823867762U;
+        static const AkUniqueID PLAY_PRAIRIE_PL = 566356829U;
+        static const AkUniqueID PLAY_SFX_KILL_RHTUK_VICTORY = 2876783754U;
+        static const AkUniqueID PLAY_SFX_KILL_SHEEP_VICTORY = 326359121U;
+        static const AkUniqueID PLAY_WATER_PL = 1488595426U;
+        static const AkUniqueID PLAY_WETLAND_PL = 263265384U;
+        static const AkUniqueID PLAY_WIND = 1020223172U;
+    } // namespace EVENTS
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID AMBIANCE_VOLUME_DESERT = 157466434U;
+        static const AkUniqueID AMBIANCE_VOLUME_PRAIRIE = 1637951615U;
+        static const AkUniqueID AMBIANCE_VOLUME_WATER = 1443029666U;
+        static const AkUniqueID AMBIANCE_VOLUME_WETLANDS = 722193395U;
+        static const AkUniqueID AMBIANCE_WIND_MOD = 1119075515U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID PLACEHOLDER = 1548734028U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID AMBIANCES = 1404066300U;
+        static const AkUniqueID DESERT = 1850388778U;
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID PRAIRIE = 237089847U;
+        static const AkUniqueID SFX = 393239870U;
+        static const AkUniqueID WATER = 2654748154U;
+        static const AkUniqueID WETLANDS = 496769563U;
+        static const AkUniqueID WIND = 1537061107U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
