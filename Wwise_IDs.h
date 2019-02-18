a0b9905c6500f9e304d90abdae3be086d66598b9
Raphaël M <raphael.marchetti.pro@gmail.com>
2019-02-18 14:43:09 +0100

    Add ship move test event and generate

--- Wwise_IDs.h
+++ Wwise_IDs.h
@@ -33,6 +33,7 @@ namespace AK
         static const AkUniqueID PLAY_SFX_KILL_RHTUK_VICTORY = 2876783754U;
         static const AkUniqueID PLAY_SFX_KILL_SHEEP_VICTORY = 326359121U;
         static const AkUniqueID PLAY_SFX_KILL_WOOD = 2180620318U;
+        static const AkUniqueID PLAY_SFX_SHIP_MOVE = 2336975836U;
         static const AkUniqueID PLAY_TAKEOFF = 955079766U;
         static const AkUniqueID PLAY_WATER_PL = 1488595426U;
         static const AkUniqueID PLAY_WETLAND_PL = 263265384U;
